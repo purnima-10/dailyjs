83c77451386b39aede58d1ea9bea3b2b7acf09aa
purnima-10 <156179575+purnima-10@users.noreply.github.com>
2024-03-26 14:55:55 +0530

    Add files via upload

---
+++ javascript/first.js
@@ -0,0 +1,10 @@
+console.log("Apna College");
+console.log("Purnima Sharma");
+fullname = "Nidhi Sharma";
+console.log(fullname);
+let name = "Alok Sharma";
+name ="Raman sharma";
+console.log(name);
+const Name = "Aman sharma";
+Name = "kunal";
+console.log(Name);
